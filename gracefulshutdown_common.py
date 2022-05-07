e83b1dae84ca19ab36b89005f5a30697f597516b
Jack Wang <weiwongfaye@hotmail.com>
2022-05-08 00:41:17 +0800

    Create gracefulshutdown_common.py

---
+++ gracefulshutdown_common.py
@@ -0,0 +1,27 @@
+import asyncio
+from signal import SIGINT, SIGTERM
+
+async def main():
+    loop = asyncio.get_running_loop()
+    for sig in (SIGTERM, SIGINT):
+        loop.add_signal_handler(sig, handler, sig)  
+
+    try:
+        while True:
+            print('<Your app is running>')
+            await asyncio.sleep(1)
+    except asyncio.CancelledError:
+        for i in range(3):
+            print('<Your app is shutting down...>')
+            await asyncio.sleep(1)
+
+def handler(sig):
+    loop = asyncio.get_running_loop()
+    for task in asyncio.all_tasks(loop=loop):  
+        task.cancel()
+    print(f'Got signal: {sig!s}, shutting down.')
+    loop.remove_signal_handler(SIGTERM)
+    loop.add_signal_handler(SIGINT, lambda: None)
+
+if __name__ == '__main__':
+    asyncio.run(main()) // more common use than method in gracefulshutdown_adv
