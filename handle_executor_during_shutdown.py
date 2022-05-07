f6f82b92427bf05c4868d261728cc1696f71a525
Jack Wang <weiwongfaye@hotmail.com>
2022-05-08 01:05:50 +0800

    Update handle_executor_during_shutdown.py

--- handle_executor_during_shutdown.py
+++ handle_executor_during_shutdown.py
@@ -33,3 +33,41 @@ try:
     asyncio.run(main())
 except KeyboardInterrupt:
     print('Bye!')
+
+
+
+
+
+# option 3: use customize executor
+
+# import time
+# import asyncio
+# from concurrent.futures import ThreadPoolExecutor as Executor
+
+# async def main():
+#     print(f'{time.ctime()} Hello!')
+#     await asyncio.sleep(1.0)
+#     print(f'{time.ctime()} Goodbye!')
+#     loop.stop()
+
+# def blocking():
+#     time.sleep(2.0)
+#     print(f"{time.ctime()} Hello from a thread!")
+
+# loop = asyncio.get_event_loop()
+# executor = Executor()  
+# loop.set_default_executor(executor)  
+# loop.create_task(main())
+# future = loop.run_in_executor(None, blocking)  
+# try:
+#     loop.run_forever()
+# except KeyboardInterrupt:
+#     print('Cancelled')
+# tasks = asyncio.all_tasks(loop=loop)
+# for t in tasks:
+#     t.cancel()
+# group = asyncio.gather(*tasks, return_exceptions=True)
+# loop.run_until_complete(group)
+# executor.shutdown(wait=True)  
+# loop.close()
+
